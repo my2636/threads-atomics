5ce06fae4fc1e73bf6aa64332f73f8e4cf094d91
m.y <myakk015@gmail.com>
2025-04-21 08:59:14 +0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    static int countToLength3 = 0;
+    static int countToLength4 = 0;
+    static int countToLength5 = 0;
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        String[] texts = new String[100_000];
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = generateText("abc", 3 + random.nextInt(3));
+        }
+
+        System.out.println(Arrays.toString(texts));
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+}
