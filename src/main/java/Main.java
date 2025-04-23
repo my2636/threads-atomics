a147ea95d52adb3866028d08f6ca44945f871268
m.y <myakk015@gmail.com>
2025-04-24 02:08:27 +0400

    отправила на проверку

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
+import java.util.stream.Collector;
+import java.util.stream.IntStream;
+
 
 public class Main {
-    static int countToLength3 = 0;
-    static int countToLength4 = 0;
-    static int countToLength5 = 0;
+    static AtomicInteger[] goodNickCounts = new AtomicInteger[]{new AtomicInteger(), new AtomicInteger(), new AtomicInteger()};
 
     public static void main(String[] args) {
         Random random = new Random();
@@ -13,7 +18,36 @@ public class Main {
             texts[i] = generateText("abc", 3 + random.nextInt(3));
         }
 
+        List<String> goodTexts = Arrays.stream(texts).filter(Main::isGoodNick).toList();
+
+        List<Predicate<String>> functions = new ArrayList<>();
+
+        functions.add(s -> s.length() == 3);
+        functions.add(s -> s.length() == 4);
+        functions.add(s -> s.length() == 5);
+
         System.out.println(Arrays.toString(texts));
+        for (Predicate<String> p : functions) {
+            int pIndex = functions.indexOf(p);
+            Runnable myRunnable = () -> {
+                for (String t : goodTexts){
+                    if (p.test(t)) {
+                        goodNickCounts[pIndex].getAndIncrement();
+                    }
+                }
+            };
+
+            myRunnable.run();
+        }
+
+        for (int i = 0; i < goodNickCounts.length; i++) {
+            System.out.printf("Красивых слов с длиной %d: %s шт\n", i + 3, goodNickCounts[i]);
+        }
+    }
+
+    static boolean isGoodNick(String s) {
+        return s.contentEquals(new StringBuilder(s).reverse()) || (s.chars().allMatch(x -> x == s.charAt(0)) ||
+                s.chars().anyMatch(x -> x != s.charAt(0)) && IntStream.range(0, s.length() - 1).allMatch(i -> s.charAt(i) <= s.charAt(i + 1)));
     }
 
     public static String generateText(String letters, int length) {
