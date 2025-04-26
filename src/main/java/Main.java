22956d1e599f54e89f8329dcd76d092982afeedf
m.y <myakk015@gmail.com>
2025-04-26 10:13:43 +0400

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,50 +1,77 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 
 public class Main {
-    static AtomicInteger[] goodNickCounts = new AtomicInteger[]{new AtomicInteger(), new AtomicInteger(), new AtomicInteger()};
+    static AtomicInteger length3 = new AtomicInteger(0);
+    static AtomicInteger length4 = new AtomicInteger(0);
+    static AtomicInteger length5 = new AtomicInteger(0);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Random random = new Random();
         String[] texts = new String[100_000];
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("abc", 3 + random.nextInt(3));
         }
 
-        List<String> goodTexts = Arrays.stream(texts).filter(Main::isGoodNick).toList();
+        Thread isPalindrome = new Thread(() -> {
+            Arrays.stream(texts)
+                    .filter((x -> x.contentEquals(new StringBuilder(x))))
+                    .forEach(Main::countGoodNickLength);
 
-        List<Predicate<String>> functions = new ArrayList<>();
+        });
 
-        functions.add(s -> s.length() == 3);
-        functions.add(s -> s.length() == 4);
-        functions.add(s -> s.length() == 5);
+        Thread isOneLetter = new Thread(() -> {
+            Arrays.stream(texts)
+                    .filter(s -> s.chars().allMatch(x -> x == s.charAt(0)))
+                    .forEach(Main::countGoodNickLength);
 
-        for (Predicate<String> p : functions) {
-            int pIndex = functions.indexOf(p);
-            new Thread(() -> {
-                for (String t : goodTexts) {
-                    if (p.test(t)) {
-                        goodNickCounts[pIndex].getAndIncrement();
-                    }
-                }
-            }).start();
-        }
+        });
 
-        for (int i = 0; i < goodNickCounts.length; i++) {
-            System.out.printf("Красивых слов с длиной %d: %s шт\n", i + 3, goodNickCounts[i]);
+        Thread isAscLetters = new Thread(() -> {
+            Arrays.stream(texts)
+                    .filter(s -> s.chars().anyMatch(x -> x != s.charAt(0))
+                            && IntStream.range(0, s.length() - 1).allMatch(i -> s.charAt(i) <= s.charAt(i + 1)))
+                    .forEach(Main::countGoodNickLength);
+
+        });
+
+        isPalindrome.start();
+        isOneLetter.start();
+        isAscLetters.start();
+
+        isAscLetters.join();
+        isOneLetter.join();
+        isPalindrome.join();
+
+
+        System.out.println("Красивых слов длиной 3: " + length3);
+        System.out.println("Красивых слов длиной 4: " + length4);
+        System.out.println("Красивых слов длиной 5: " + length5);
+    }
+
+    static void countGoodNickLength(String s) {
+        switch (s.length()) {
+            case 3:
+                System.out.println(s.length() + "длина");
+                System.out.println(s);
+                System.out.println(length3.incrementAndGet());
+                break;
+            case 4:
+                System.out.println(s.length() + "длина");
+                System.out.println(s);
+                length4.incrementAndGet();
+                break;
+            case 5:
+                System.out.println(s.length() + "длина");
+                System.out.println(s);
+                length5.incrementAndGet();
+                break;
         }
     }
 
-    static boolean isGoodNick(String s) {
-        return s.contentEquals(new StringBuilder(s).reverse()) || (s.chars().allMatch(x -> x == s.charAt(0)) ||
-                s.chars().anyMatch(x -> x != s.charAt(0)) && IntStream.range(0, s.length() - 1).allMatch(i -> s.charAt(i) <= s.charAt(i + 1)));
-    }
 
     public static String generateText(String letters, int length) {
         Random random = new Random();
